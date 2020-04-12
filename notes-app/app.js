d1d84ace277d4bc9b59c38c9c9aa6ca2a6e1151b
Joan Sanchez <jsanchezgarcia13@gmail.com>
2020-04-13 00:40:44 +0200

    working with yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,56 @@
-const getNotes = require('./notes')
 const chalk = require('chalk')
+const yargs = require('yargs')
+const getNotes = require('./notes')
 
-const message = getNotes()
-console.log(message)
-console.log(chalk.red('Success!'));
-console.log(chalk.green.bold.bgRed('Success!'));
+//Customize yargs version
+yargs.version('1.1.0')
+
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder:{
+        title:{
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body:{
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log('Title: ' + argv.title + '\n' + 'Body: ' + argv.body)
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing a new note!')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function(){
+        console.log('Llisting all notes!')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log('Reading a note!')
+    }
+})
+
+yargs.parse()
 
 
 
