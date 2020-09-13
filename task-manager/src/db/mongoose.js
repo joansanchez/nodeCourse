11f18d0de9fe913df073cc2bad1a299acd5fb811
joansanchez <jsanchezgarcia13@gmail.com>
2020-09-13 18:00:59 +0200

    creating models with mongoose and using validator package

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager-api'
@@ -10,16 +11,34 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type:String
+        type: String,
+        required: true,
+        trim: true
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) throw new Error ('Email is invalid')
+        }
     }
 })
 
 const Joan = new User({
-    name: 'Joan',
-    age: 22
+    name: 'Mike Andrew',
+    email: 'joan@jo.com' 
+    
 })
 
 Joan.save().then((result)=>{
@@ -27,3 +46,23 @@ Joan.save().then((result)=>{
 }).catch((error) => {
     console.log(error)
 })
+
+/* const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+})
+
+const task = new Task ({
+    description: 'Llamar a Jazztel',
+    completed: false
+})
+
+task.save().then((result) => {
+    console.log(task);
+}).catch((error) => {
+    console.log(error);
+}) */
